984411f7044d403eaab856e9818418c3692f59dc
etombert <you@example.com>
2017-02-19 23:58:57 +0100

    section 1 presque terminée

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,1 +1,7 @@
-/*INSERT YOUR JAVASCRIPT HERE WALLAH*/
+$(document).ready(function(){
+	$('.tag--menu').click(function(e){
+		e.preventDefault();
+		$('.nav__left').animate({width:'toggle'},350);
+		$('body').toggleClass('hidden');
+	})
+})
