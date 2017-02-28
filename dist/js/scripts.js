7e142e7dd5fd27abdca5696d34401c3c22db9027
etombert <you@example.com>
2017-03-01 00:33:07 +0100

    finished

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,3 +1,14 @@
+//SCROLL TO
+$(document).ready(function() {
+	$('.scrollTo').click( function() { // Au clic sur un élément
+		var page = $(this).attr('href'); // Page cible
+		var speed = 1000; // Durée de l'animation (en ms) (750 à la base)
+		$('html, body').animate( { scrollTop: $(page).offset().top }, speed ); // Go
+		return false;
+	});
+});
+
+// MENU
 $(document).ready(function(){
 	$('.tag--menu, .cross').click(function(e){
 		e.preventDefault();
